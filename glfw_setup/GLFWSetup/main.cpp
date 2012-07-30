95d93a320f8ed2d67509a3eeebe408443b605023
upei <upei@iTinto.local>
2012-07-29 23:23:33 -0300

    Added basic GLFW setup app

---
+++ glfw_setup/GLFWSetup/main.cpp
@@ -0,0 +1,73 @@
+//========================================================================
+// Creates a GL 3.2 720p window and clears it
+//========================================================================
+#define GLFW_INCLUDE_GL3
+#define GLFW_NO_GLU
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glfw.h>
+
+int main( void )
+{
+    int width, height, x;
+    double t;
+    
+    // Initialise GLFW
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+    
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 2);
+    glfwOpenWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    // Open a window and create its OpenGL context
+    if( !glfwOpenWindow( 1280, 720, 0,0,0,0, 0,0, GLFW_WINDOW ) )
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+    
+    glfwSetWindowTitle( "Setup" );
+    
+    // Ensure we can capture the escape key being pressed below
+    glfwEnable( GLFW_STICKY_KEYS );
+    
+    // Enable vertical sync (on cards that support it)
+    glfwSwapInterval( 1 );
+    
+    do
+    {
+        t = glfwGetTime();
+        glfwGetMousePos( &x, NULL );
+        
+        // Get window size (may be different than the requested size)
+        glfwGetWindowSize( &width, &height );
+        
+        // Special case: avoid division by zero below
+        height = height > 0 ? height : 1;
+        
+        glViewport( 0, 0, width, height );
+        
+        // Clear color buffer to black
+        glClearColor( 0.4f, 0.4f, 0.4f, 0.0f );
+        glClear( GL_COLOR_BUFFER_BIT );
+        
+        // Swap buffers
+        glfwSwapBuffers();
+        
+    } // Check if the ESC key was pressed or the window was closed
+    while( glfwGetKey( GLFW_KEY_ESC ) != GLFW_PRESS &&
+          glfwGetWindowParam( GLFW_OPENED ) );
+    
+    // Close OpenGL window and terminate GLFW
+    glfwTerminate();
+    
+    exit( EXIT_SUCCESS );
+}
+
